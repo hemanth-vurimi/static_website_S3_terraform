d75581fd33998f7002468300ee6cbead4f9101a1
hemanth-vurimi <80247546+hemanth-vurimi@users.noreply.github.com>
2024-09-11 14:27:10 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,77 @@
+resource "aws_s3_bucket" "mybucket" {
+    bucket = var.bucketname
+  
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example-1" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "my-acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example-1,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.mybucket.id
+  key = "index.html"
+  source = "index.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.mybucket.id
+  key = "error.html"
+  source = "error.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "profile" {
+  bucket = aws_s3_bucket.mybucket.id
+  key = "profile.png"
+  source = "profile.png"
+  acl = "public-read"
+}
+
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+   depends_on = [ aws_s3_bucket_acl.my-acl ]
+}
+
+
+output "website_url" {
+  value = aws_s3_bucket.mybucket.website_endpoint
+}
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "bucketname" {
+  default = "demo-s3-bucket-upload-1234"
+}
